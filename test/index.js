da44385dfdf5d64165dfa0884fbe498cfe06f52d
Michael Kastner <kastner@galt.de>
2019-11-26 13:13:11 +0100

    better error handling

--- test/index.js
+++ test/index.js
@@ -1,14 +1,14 @@
 const tape = require('tape');
-// const log = require('mk-log');
+//const log = require('mk-log');
 const EnvVars = require('../lib/index.js');
 
-tape('test app deploy', function(t) {
+tape('test envVars deploy', function(t) {
 
-  const app = EnvVars({ app: 'USERS', deploy: 'PRODUCTION' });
+  const envVars = EnvVars({ app: 'USERS', deploy: 'TEST' });
 
   // vars defined in package.json test
-  t.equals(app.forName('NAME'), 'JohnGalt');
-  t.equals(app.forName('PASSWORD'), '0000');
+  t.equals(envVars.forName('NAME'), 'JohnGalt');
+  t.equals(envVars.forName('PASSWORD'), '0000');
 
   t.end();
 
@@ -16,11 +16,42 @@ tape('test app deploy', function(t) {
 
 tape('test custom app deploy', function(t) {
 
-  const app = EnvVars({ custom: 'GALT', app: 'USERS', deploy: 'PRODUCTION' });
+  const envVars = EnvVars({ custom: 'GALT', app: 'USERS', deploy: 'TEST' });
 
   // vars defined in package.json test
-  t.equals(app.forName('PASSWORD'), '0000');
+  t.equals(envVars.forName('PASSWORD'), '0000');
   t.end();
 
 });
 
+tape('test throws error because of env is matching env vars deploy', function(t) {
+
+  const envVars = EnvVars({ custom: 'GALT', app: 'USERS', deploy: 'TEST' });
+
+  // vars defined in package.json test
+  try { 
+    envVars.forName('NOT_THERE');
+  } catch (err) {
+    t.ok(err.toString().match(/GALT_USERS_NOT_THERE_TEST/));
+  } finally {
+    t.end();
+  }
+  
+
+});
+
+tape('test does not throw error because of wrong env', function(t) {
+
+  const envVars = EnvVars({ custom: 'GALT', app: 'USERS', deploy: 'TEST' });
+
+  // vars defined in package.json test
+  try { 
+    envVars.forName('NOT_THERE');
+  } catch (err) {
+    t.ok(err.toString().match(/GALT_USERS_NOT_THERE_TEST/));
+  } finally {
+    t.end();
+  }
+  
+
+});
